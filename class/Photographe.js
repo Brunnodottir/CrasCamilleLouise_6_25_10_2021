508544fc3506e3754f3b4c63b722c55493323097
Camille-Louise <brunnodottir@gmail.com>
2021-10-05 10:06:50 +0200

    add missing files

---
+++ class/Photographe.js
@@ -0,0 +1,95 @@
+class Photographe {
+    constructor(nom, id, city, country, tags, quote, price, portrait) {
+        this.name = nom;
+        // this.firstname = nom.split(" ")[0];
+        // this.lastname = nom.split(" ") [1];
+        this.id = id;
+        this.city = city;
+        this.country = country;
+        this.tags = tags;
+        this.quote = quote;
+        this.price = price;
+        this.portrait = portrait;
+    }
+
+
+
+    render(){
+        const container = document.createElement("div");
+        container.className = "main-photographes";
+        container.title = "Page-photographe";  
+        // container.href = "/page.html?id="+this.id; //+ this.id// OR SEARCH PARAMS OR ?id=${photographer.id}
+        document.body.appendChild(container); 
+
+        document.querySelector("main").append(container);
+ 
+        
+     
+        const linkcontainer = document.createElement('a');
+        linkcontainer.className = "photographer-data"
+        linkcontainer.href ="/page.html?id="+this.id;
+        linkcontainer.setAttribute('alt', this.name);
+        // linkcontainer.setAttribute("tab-index=0")
+        // linkcontainer.aria-labellby="test-acces";
+        const img = document.createElement('img');
+        img.src = "Sample Photos/Photographers ID Photos/" + this.portrait;
+        img.className = "photographe-img";
+        img.href ="/page.html?id="+this.id;
+        const h2 = document.createElement("h2");
+        h2.className = "photographe-name";
+        h2.textContent = this.name;
+        h2.setAttribute("aria-label", "cliquer pour accéder à la page photographe");
+        const loc = document.createElement ("p");
+        loc.className = "photographe-loc";
+        loc.textContent = this.city +', '+ this.country;
+        const quote = document.createElement("p");
+        quote.className ="photographe-quote";
+        quote.textContent = this.quote;
+        const pprice = document.createElement ("p");
+        pprice.className ="photographe-tarif";
+        pprice.textContent = this.price;
+        
+        linkcontainer.append(img, h2, loc, quote, pprice);
+        container.append(linkcontainer);
+        const ptags = document.createElement("div");
+        ptags.className = "photographe-tags";
+        
+
+
+        for (let i = 0 ; i < this.tags.length ; i++) {
+           
+            const span = document.createElement ("a");
+            span.href ="./index.html?tag="+this.tags[i];
+            span.className ="firsttag";
+            span.textContent = this.tags[i];
+            span.setAttribute("aria-label", "Cliquer pour afficher la catégorie " +this.tags[i])
+            ptags.append(span)
+            container.append(ptags);
+
+        }
+
+
+     
+        
+
+        
+
+    }
+
+    
+}
+
+
+
+// class Artist extends Photographe {
+//     constructor(nom, id, city, country, tags, quote, price, portrait) {
+//         super(nom, id, city, country, tags, quote, price, portrait) 
+            
+//     }
+
+   
+// }
+// const artiste1 = new Artist ("Michel", 140, "Paris", "France", [null], 150);
+// console.log(artiste1);
+
+export {Photographe};
---
+++ class/media.js
@@ -0,0 +1,362 @@
+import {getTotalLike, updateTotalLike} from "../main_photographer.js";
+
+class Media {
+    constructor (id, photographerId, title, src, tags, likes, date,price){
+        this.id = id;
+        this.photographerId = photographerId;
+        this.title = title;
+        this.src = src;
+        this.tags = tags;
+        this.likes = likes;
+        this.date = date;
+        this.price = price;
+    }
+
+
+    // likeSystem(){
+    //     console.log('likesystem');
+    //     let liked = false;
+
+    //     if(this.likes == this.likes){
+    //         this.incrLikes()
+    //     }else {
+    //         this.decrLikes()
+    //     }
+
+
+
+     
+    // }
+
+    incrLikes(){
+        // const thislike = document.querySelectorAll('i');
+        // thislike.setAttribute('true','true');
+
+        this.likes = this.likes + 1;
+       
+
+
+    }
+
+    decrLikes(){
+        this.likes = this.likes -1;
+    }
+    // ajout methode rafraichissement ?
+
+
+}
+
+class Image extends Media {
+    constructor(id, photographerId, title, src, tags, likes, date,price){
+        super(id, photographerId, title, src, tags, likes, date,price)
+    }
+
+    render(){
+        const container = document.createElement("div");
+    
+        container.className = "media";
+                    // addEventListener openLightbox ?
+
+        document.body.appendChild(container);
+
+        const img = document.createElement("img");
+        img.src = this.src; 
+        img.addEventListener("click", (e) => {
+            this.handleClick();
+        })
+        img.className = "media_img";
+        img.setAttribute("aria-label", "Cliquer pour agrandir")
+        const infos = document.createElement("div");
+        infos.className = "media_infos";
+        const h2 = document.createElement ('h2');
+        h2.className = "media_title";
+        h2.textContent = this.title;
+        const likes = document.createElement("div");
+        likes.className = "media_likes";
+        const numberoflikes = document.createElement ("p");
+        numberoflikes.className = "media_numberoflikes"
+        numberoflikes.textContent = this.likes;
+        const heart = document.createElement ("i");
+        heart.className = "fas fa-heart";
+        heart.setAttribute("aria-label", "cliquer pour aimer le média")
+       
+        // let liked = false;
+        // console.log(liked);
+        heart.addEventListener("click", () => {
+            // heart.setAttribute('true', 'true')
+            // console.log(heart);
+            // this.likeSystem(); 
+
+
+            this.incrLikes();  
+            numberoflikes.textContent = this.likes;
+            updateTotalLike(getTotalLike());
+            
+        })
+        // heart.addEventListener("keydown", () => {
+        //     this.incrLikes();  
+        //     numberoflikes.textContent = this.likes;
+        //     updateTotalLike(getTotalLike());
+        // })
+        
+        infos.append( h2,likes );
+        likes.append(numberoflikes, heart);
+
+    
+        
+        container.append(img,infos);
+        document.querySelector("#medias").append(container);
+
+
+    }
+
+    renderLightbox(){
+        const container = document.getElementsByClassName("lightbox___container")[0];
+        const media = document.createElement('img');
+        media.src = this.src;
+        media.className = "media_lightbox";
+        const h2 = document.createElement('h2');
+        h2.className = "media_lightbox_title";
+        h2.textContent = this.title;
+
+        container.append(media, h2)
+        
+
+    }
+
+    
+    }
+
+
+
+    class Video extends Media {
+        constructor(id, photographerId, title, src, tags, likes, date,price){
+            super(id, photographerId, title, src, tags, likes, date,price)
+        }
+        render(){
+            const container = document.createElement("div");
+            
+            container.className = "media";
+            document.body.appendChild(container);
+
+            const video = document.createElement("video");
+            video.src = this.src;
+            video.addEventListener("click", (e) => {
+                this.handleClick();
+            })
+            video.className ="media_video";
+            video.setAttribute("aria-label", "Cliquer pour agrandir")
+
+            const infos = document.createElement("div");
+            infos.className = "media_infos";
+            const h2 = document.createElement ('h2');
+            h2.className = "media_title";
+            h2.textContent = this.title;
+            const likes = document.createElement("div");
+            likes.className = "media_likes";
+            const numberoflikes = document.createElement ("p");
+            numberoflikes.className = "media_numberoflikes"
+            numberoflikes.textContent = this.likes;
+           const heart = document.createElement ("i");
+           heart.className = "fas fa-heart";
+           heart.setAttribute("aria-label", "cliquer pour aimer le média")
+           heart.addEventListener("click", () => {
+               this.incrLikes();
+               numberoflikes.textContent = this.likes;
+               updateTotalLike(getTotalLike());
+           })
+           infos.append( h2,likes);
+           likes.append(numberoflikes, heart);
+
+    
+        
+        container.append(video,infos);
+        document.querySelector("#medias").append(container);
+
+
+
+
+        }
+        renderLightbox(){
+            const container = document.getElementsByClassName("lightbox___container")[0];
+            const media = document.createElement('video');
+            media.src = this.src;
+            media.className = "media_lightbox";
+            const h2 = document.createElement('h2');
+            h2.className = "media_lightbox_title";
+            h2.textContent = this.title;
+    
+            container.append(media, h2)
+            
+    
+        }
+    
+        }
+
+
+class Lightbox {
+ 
+
+    constructor(mediaList) {
+        this.mediaList = mediaList;
+        this.activeMedia = null;
+        this.mediaList.forEach((el) =>  {
+            el.handleClick = () => {
+                this.render(el);  // pourquoi ?
+                // el.handleKe
+            }
+        });
+
+        this.closeIcon = null;
+        this.leftIcon = null;
+        this.rightIcon = null;
+        this.onKeyup = this.onKeyup.bind(this)
+        document.addEventListener('keyup', this.onKeyup.bind(this))
+    }
+
+
+    
+        
+    
+
+    render(inputMedia){
+        console.log(this.mediaList);
+        // const mediaData = this.mediaList.find(el => el.id === this.id);
+        this.activeMedia = inputMedia;
+        const mediaData = this.activeMedia;
+        console.log(mediaData);
+        const container = document.createElement("div");
+        container.className ="lightbox___container";
+        document.body.appendChild(container);
+        this.closeIcon = document.createElement("i");
+        this.closeIcon.className = "lightbox__close fas fa-times closeIcon";
+        this.closeIcon.setAttribute("aria-label", "Fermer")
+        this.closeIcon.addEventListener("click", () => {
+            this.close(); // tout ce qui est déclaré dans la classe peut devenir this.something
+        })
+        this.leftIcon = document.createElement("i");
+        this.leftIcon.className = "lightbox__prev fas fa-chevron-left leftIcon";
+        this.leftIcon.setAttribute("aria-label", "Aller au média précédent")
+        this.leftIcon.addEventListener("click", () => {
+            this.prev();
+        })
+        this.rightIcon = document.createElement("i");
+        this.rightIcon.className = "lightbox__next fas fa-chevron-right rightIcon";
+        this.rightIcon.setAttribute("aria-label", "Aller au média suivant")
+
+        this.rightIcon.addEventListener("click", () => {
+            this.next();
+        })
+
+       
+       container.append(this.closeIcon, this.leftIcon, this.rightIcon)
+        
+       this.activeMedia.renderLightbox();
+       
+
+       
+    }
+    
+
+    
+
+    next() {
+    //    const activeMedia = this.media.filter(media => name)
+    console.log("next");
+
+    const idx = this.mediaList.findIndex((e)=> {
+        return e === this.activeMedia});
+        console.log(idx);
+        console.log("active media : ", this.activeMedia);
+
+        document.querySelector(".media_lightbox").remove();
+        document.querySelector(".media_lightbox_title").remove();
+
+
+        if (idx >= this.mediaList.length -1) {
+            this.activeMedia = this.mediaList[0];
+            this.activeMedia.renderLightbox();
+        }
+        else {
+        this.activeMedia = this.mediaList[idx+1]
+        this.activeMedia.renderLightbox();}
+
+     
+    
+        
+    
+    }
+
+
+
+    
+
+    prev() {
+        const idx = this.mediaList.findIndex((e)=> {
+            return e === this.activeMedia});
+            console.log(idx);
+            console.log("active media : ", this.activeMedia);
+    
+            document.querySelector(".media_lightbox").remove();
+            document.querySelector(".media_lightbox_title").remove();
+    
+    
+            if (idx === 0 ) {
+                const newIndex = this.mediaList.length -1;
+                this.activeMedia = this.mediaList[newIndex];
+                this.activeMedia.renderLightbox();
+            }
+            else {
+            this.activeMedia = this.mediaList[idx-1]
+            this.activeMedia.renderLightbox();}
+    
+
+    }
+
+    open(){
+        document.getElementsByClassName("lightbox___container").style.display = 'block';
+
+
+    }
+
+    close() {
+        // const closeBtn = document.querySelector(".closeIcon");
+        const lightbox = document.querySelector(".lightbox___container");
+
+        lightbox.remove();
+        // document.removeEventListener('keyup', this.onKeyup)
+        // this.closeIcon.addEventListener("click", close)
+
+        
+
+    }
+
+    
+
+        
+onKeyup(e) {
+    if (e.key === 'Escape'){
+        this.close(e);
+    }
+
+    else if(e.key === 'ArrowLeft'){
+        this.prev(e)
+    }
+    else if (e.key === 'ArrowRight'){
+        this.next(e)
+    }
+
+    else if (e.key === 8){
+        this.open(e);
+    }
+
+    
+
+}
+
+
+    
+}
+
+
+export {Media, Image, Video, Lightbox};
